6e7a9346b9c812a38baa07c1411fae8dbb814c26
Bob <ganesha.shridhara.bhatt@oracle.com>
2023-08-21 12:21:00 +0530

    Initial push

---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "DigitalDairy"
+include(":app")
+include(":Labour")
+include(":LandDetails")
+include(":Schedules")
+include(":vault")
